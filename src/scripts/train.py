0acf81efe0d5c210a783cac50265d0b3e09e20f8
Michal Pogoda <michalpogoda@hotmail.com>
2019-05-20 22:46:08 +0200

    Dodanie czasowego treningu

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -12,10 +12,13 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--alpha", default=1e-4, type=float, help="Training alpha rate")
     parser.add_argument("-e", "--epochs", default=10, type=int, help="Number of training epochs")
     parser.add_argument("-f", "--frequent_save", help="Save model on every epoch", action="store_true")
+    parser.add_argument("-ts", "--time_save", default=0, type=float, help="Save model every N minutes")
+    parser.add_argument("-tk", "--time_kill", default=0, type=float, help="Kill training after N minutes")
 
     args = parser.parse_args()
 
     trainer = Trainer(batch_size=args.batch_size, alpha=args.alpha, images_path=args.images_path,
                       annotations_path=args.annotations_path, model_path=args.model_path, max_epoch=args.epochs,
-                      save_every_epoch=args.frequent_save)
+                      save_every_epoch=args.frequent_save, save_after_minutes=args.time_save, 
+                      kill_after_minutes=args.time_kill)
 
--- src/training/Trainer.py
+++ src/training/Trainer.py
@@ -3,12 +3,14 @@ from dataset_generation.data_feeder import TrainImageGenerator
 from dataset_generation.augmenter import NoAgumenter
 import tensorflow as tf
 from neural_network.network_constants import FEATURE_MAPS
+from datetime import datetime  
+from datetime import timedelta  
 
 
 class Trainer:
     def __init__(self, batch_size=8, alpha=1e-4, images_path="../../datasets/micro",
                  annotations_path="../../datasets/micro/annotations.csv", model_path="models/model.ckpt", max_epoch=10,
-                 num_early_stopping=3, save_every_epoch=True):
+                 num_early_stopping=3, save_every_epoch=True, save_after_minutes=0, kill_after_minutes=0):
         self.batch_size = batch_size
         self.alpha = alpha
         self.images_path = images_path
@@ -16,6 +18,8 @@ class Trainer:
         self.output_model_path = model_path
         self.max_epoch = max_epoch
         self.save_every_epoch = save_every_epoch
+        self.save_after_minutes = save_after_minutes
+        self.kill_after_minutes = kill_after_minutes
 
     def fit(self):
         gen = TrainImageGenerator(self.annotations_path, self.images_path,
@@ -33,6 +37,9 @@ class Trainer:
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
+            training_start_time = datetime.now()
+            last_epoch_save_time = datetime.now()
+
             for i in range(self.max_epoch):
                 for batch_x, batch_y in gen.get_batches_in_epoch():
                     calc_loss, _ = sess.run([loss, minimizer], {xs: batch_x, **dict(zip(ys, batch_y))})
@@ -40,5 +47,16 @@ class Trainer:
 
                 if self.save_every_epoch:
                     saver.save(sess, self.output_model_path + f"-epoch{i}")
+
+                now = datetime.now()
+
+                # Periodic saving
+                if (save_after_minutes > 0) and (now > last_epoch_save_time + timedelta(minutes=save_after_minutes)):
+                    saver.save(sess, self.output_model_path + f"-epoch{i}")
+                    last_epoch_save_time = last_epoch_save_time + timedelta(minutes=save_after_minutes)
+
+                # Ending training after time
+                if (kill_after_minutes > 0) and (now > training_start_time + timedelta(minutes=self.kill_after_minutes)):
+                    break
 
             saver.save(sess, self.output_model_path + "-final")
